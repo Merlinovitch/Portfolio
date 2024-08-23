309cce95558f25adade76f066f35b2e0f7f405bc
Merlinovitch <a.merlin92@gmail.com>
2024-08-23 16:36:37 +0200

    update descriptions under projects

--- src/pages/Article.tsx
+++ src/pages/Article.tsx
@@ -2,15 +2,16 @@
 
 interface ArticleProps {
     title: string;
+    modal?: string;
     description: string;
     imgUrl: string;
     alt: string;
     stacks: string[];
     link?: string,
-    github?: string
+    github?: string 
 }
 
-function Article( { title, description, imgUrl, alt, stacks, link, github }: ArticleProps ) {
+function Article( { title, description, imgUrl, alt, stacks, link, github, modal }: ArticleProps ) {
     return (
       <div>
         <article className="projects__card">
@@ -18,13 +19,24 @@ function Article( { title, description, imgUrl, alt, stacks, link, github }: Art
           <div className="projects__modal">
             <span className="projects__subtitle">{stacks.join(' / ')}</span>
             <h2 className="projects__title">
-              {description}
+              {modal}
             </h2>
             <a href={link} target="_blank" className="projects__button">
               {title} <i className="ri-external-link-line"></i>
             </a>
           </div>
         </article>
+        <p className="projects__description" dangerouslySetInnerHTML={{ __html: description }}></p>
+        {github && (
+        <a
+          href={github}
+          className="projects__button"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Voir sur GitHub
+        </a>
+      )}
       </div>
     );
   }
--- src/pages/ProjectList.ts
+++ src/pages/ProjectList.ts
@@ -1,38 +1,41 @@
 export const projects = [
     {
         title: "SplitR",
-        description: "Projet Fil Rouge CDA, Application de partage d'addition au restaurant",
+        modal: "Projet Fil Rouge CDA, Application de partage d'addition au restaurant",
         imgUrl: "assets/img/splitr.webp",
         alt: "projet splitr",
         stacks: ["Angular", "Java"],
         link: "https://preprod.splitr.alexandre-merlin.com/",
-        github: "https://github.com/Merlinovitch/SplitR/"
+        github: "https://github.com/Merlinovitch/SplitR/",
+        description: "SplitR, une application permettant de diviser l'addition entre les participants.Le restaurateur peut créer son menu, son plan de table, et assigner les plats aux différentes tables. Ensuite, les participants peuvent scanner un QR code leur permettant de chacun s'attribuer les plats qu'ils souhaitent payer. Le projet est encours de développement."
     },
     {
         title: "AstroSS",
-        description: "Le système solaire en 3D avec React",
+        modal: "Le système solaire en 3D avec React",
         imgUrl: "assets/img/astross.webp",
         alt: "projet astross",
-        stacks: ["React"],
+        stacks: ["React","Three.js"],
         link: "https://astro-systeme-solaire-ass.vercel.app/",
-        github: "https://github.com/Merlinovitch/AstroSS"
+        github: "https://github.com/Yokokorico/Astro-Systeme-Solaire-ASS",
+        description: "Un projet en React fait avec 3 autres développeurs. Création d'une vue en 3D du système solaire avec la librairie Three.js et dreijs-fiber. Vue d'ensemble du système solaire, focus sur chaque planête, vitesse ajustable, toutes les informations (vitesse de rotation, révolution, taille de la planète, inclinaision, ...) sont tirées de l'api : <a href='https://api.le-systeme-solaire.net/' target='_blank'>le-systeme-solaire.net</a>"
     },
     {
         title: "SkillTree",
-        description: "Arbre de compétence pour la formation CDA Simplon",
+        modal: "Arbre de compétence pour la formation CDA Simplon",
         imgUrl: "assets/img/skilltree.png",
         alt: "projet skilltree",
         stacks: ["Angular"],
         link: "https://2024-cda-boulogne.github.io/skilltree-tdu/",
-       
+        description: "Un projet avec 2 autres développeurs, C'est un arbre de compétence permettant de visualiser l'avancée des compétences des apprenants de la formation CDA Simplon sous forme d'arbre de compétences inspiré de la science-fiction."       
     },
     {
         title: "Mariloo",
-        description: "Création du formulaire de contact et ajout du LazyLoading de la page Blog",
+        modal: "Création du formulaire de contact et ajout du LazyLoading de la page Blog",
         imgUrl: "assets/img/mariloo.png",
         alt: "projet mariloo",
         stacks: ["Angular"],
         link: "https://mariloo.com/contact",
+        description: "Refonte de la page contact du site <a href='https://mariloo.com/contact' target='_blank'>mariloo.com</a> et ajout du LazyLoading de la page Blog et refacto du code."
        
     },
 
